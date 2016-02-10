fd595432a6b8de0ac73b24f52f539a3834355ed5
Christian <christian@playlogix.com>
2016-02-10 13:57:03 +0200

    added entity_dump and user_dump for dummy data

---
+++ sql/entity_dump.sql
@@ -0,0 +1,3 @@
+/*add dummy entity*/
+INSERT INTO entity (entity_address, entity_email, entity_logo, entity_name) VALUES ("41 Buitendag street, Monte Vista, 7460", "chris@worldbank.com", "http://img13.deviantart.net/083d/i/2010/338/a/a/animated_sprite_sheet_32x32_by_digibody-d3479l2.gif", "World Bank")
+
