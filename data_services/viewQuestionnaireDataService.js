e15b783dba73dbba1aef4bb15477408d225bd892
mshava <Sakhile@projectCodeX.co>
2016-02-26 15:40:44 +0200

    updated view questionnaire

--- data_services/viewQuestionnaireDataService.js
+++ data_services/viewQuestionnaireDataService.js
@@ -2,14 +2,6 @@ const QueryService = require("../data_services/query-service");
 
 module.exports = function(connection) {
     const queryService = new QueryService(connection);
-
-    //this.fetchEntityMetrics = function() {
-    //    return queryService.executeQuery('SELECT * FROM metric WHERE entity_id = 1'); //Todo entity hard coded until params in url
-    //};
-    //
-    //this.addMetricToMetricTable = function(data) {
-    //    return queryService.executeQuery('INSERT INTO metric set ?', data);
-    //};
     this.showQuestionnaires = function(id) {
       return queryService.executeQuery('SELECT id, entity_id, name, DATE_FORMAT(dueDate,"%d %b %y") as dueDate FROM questionnaire WHERE parent_questionnaire_id != \'null\' and entity_id = ?', id)
     };
