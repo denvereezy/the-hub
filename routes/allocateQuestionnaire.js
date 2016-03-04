178720990369d94b435c39fc1b3754337f86c895
denver <denver@projectcodex.co>
2016-03-04 09:54:49 +0200

    removed console.log

--- routes/allocateQuestionnaire.js
+++ routes/allocateQuestionnaire.js
@@ -76,7 +76,6 @@ exports.allocateToSubEntity = function(req, res, next){
       .then(function(result){
           var questionnaireId = result.insertId;
           var metric_ids = data.metric_ids.map(function(m){return {metric_id : m}});
-          console.log('metric' + metric_ids);
           return allocateQuestionnaireDataService.allocateMetricListToQuestionaire(questionnaireId, metric_ids);
       })
           .then(function(results){
