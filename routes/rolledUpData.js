6cd43c06942be1c767bf8c842f25c3e0b3c14d9d
denver <denver@projectcodex.co>
2016-03-07 13:44:52 +0200

    added function to show results of questions answered

---
+++ routes/rolledUpData.js
@@ -0,0 +1,18 @@
+exports.show = function (req, res, next) {
+   req.getServices()
+       .then(function(services){
+           const rollupDataService = services.rollupDataService;
+           var base_questionnaire_id = req.body.base_questionnaire_id;
+           rollupDataService.rollupMetrics(base_questionnaire_id)
+               .then(function(values){
+                   res.render('', {
+                       values : values,
+                       user:req.session.user,
+                       entity:req.session.entity
+                   });
+               });
+       })
+       .catch(function(err){
+           next(err);
+       });
+};
