e39f4ad8f8d9cf93b2b171542d455a433aff0617
denver <denver@projectcodex.co>
2016-03-07 13:28:24 +0200

    added query for rollup data service

--- data_services/rollupDataService.js
+++ data_services/rollupDataService.js
@@ -2,7 +2,10 @@ const QueryService = require('../data_services/query-service');
 
 module.exports = function (connection) {
 const queryService = new QueryService(connection);
-this.rollupMetrics = function(){
-       return queryService.executeQuery();
+this.rollupMetrics = function(base_questionnaire_id){
+       return queryService.executeQuery(
+         `SELECT name, sum(value) as value, title, description
+          FROM questionnaire inner join questionnaire_metric on questionnaire_id = questionnaire.id
+           inner join metric on questionnaire_metric.metric_id = metric.id WHERE base_questionnaire_id = ? group by metric_id`, base_questionnaire_id);
    };
 };
