b8e69106a5a92390595bb2dd63090685ae7ce75b
denver <denver@projectcodex.co>
2016-03-08 13:26:27 +0200

    added metricResults function

--- routes/questions.js
+++ routes/questions.js
@@ -15,14 +15,47 @@ const facilitator = req.session.type === 'Facilitator';
             Promise.join(setupQuestionnaireDataService.getQuestionnaireById(id),
                           questionDataService.showAll(id),
                           allocateQuestionnaireDataService.showEntities(entity_id),
-                          rollupDataService.rollupMetrics(base_questionnaire_id),
-            function(questionnaire, questions, entities, values){
+            function(questionnaire, questions, entities){
                   res.render('view-questions', {
                       questionnaire  : questionnaire,
                       questions  : questions,
                       entities  : entities,
                       questionnaire_id  : id,
+                      donor:donor,
+                      facilitator:facilitator,
+                      user:req.session.user,
+                      entity:req.session.entity
+                  });
+          });
+      })
+      .catch(function(err){
+          next(err);
+      });
+};
+
+exports.metricResults = function (req, res, next) {
+var id = req.params.id;
+var entity_id = req.session.entity_id;
+var base_questionnaire_id = req.params.base_questionnaire_id;
+const donor = req.session.type === 'Donor';
+const facilitator = req.session.type === 'Facilitator';
+  req.getServices()
+      .then(function(services){
+          const questionDataService = services.questionDataService;
+          const setupQuestionnaireDataService = services.setupQuestionnaireDataService;
+          const allocateQuestionnaireDataService = services.allocateQuestionnaireDataService;
+          const rollupDataService = services.rollupDataService;
+            Promise.join(setupQuestionnaireDataService.getQuestionnaireById(id),
+                          questionDataService.showAll(id),
+                          allocateQuestionnaireDataService.showEntities(entity_id),
+                          rollupDataService.rollupMetrics(base_questionnaire_id),
+            function(questionnaire, questions, entities, values){
+                  res.render('questionnaire-results', {
+                      questionnaire  : questionnaire,
+                      questions  : questions,
+                      entities  : entities,
                       values : values,
+                      questionnaire_id  : id,
                       donor:donor,
                       facilitator:facilitator,
                       user:req.session.user,
