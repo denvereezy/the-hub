95fbdd582f22a5c745b391d2d880fcaf8b60b98b
denver <denver@projectcodex.co>
2016-03-09 13:04:37 +0200

    ordered questionnaires by latest first

--- data_services/viewQuestionnaireDataService.js
+++ data_services/viewQuestionnaireDataService.js
@@ -3,6 +3,6 @@ const QueryService = require("../data_services/query-service");
 module.exports = function(connection) {
     const queryService = new QueryService(connection);
     this.showQuestionnaires = function(id) {
-      return queryService.executeQuery('SELECT id, entity_id, name,base_questionnaire_id, DATE_FORMAT(dueDate,"%d %b %y") as dueDate FROM questionnaire WHERE parent_questionnaire_id != \'null\' and entity_id = ?', id)
+      return queryService.executeQuery('SELECT id, entity_id, name,base_questionnaire_id, DATE_FORMAT(dueDate,"%d %b %y") as dueDate FROM questionnaire WHERE parent_questionnaire_id != \'null\' and entity_id = ? order by id desc', id)
     };
 };
