9a73d677515b8d904c836af0547fd51dfba1c169
denver <denver@projectcodex.co>
2016-03-11 19:34:59 +0200

    unused code removed & cleaned up

--- data_services/answerDataService.js
+++ data_services/answerDataService.js
@@ -4,12 +4,11 @@ module.exports = function (connection) {
   const queryService = new QueryService(connection);
 
   this.answeredMetrics = function(data,questionnaire_metric_id){
-       return queryService.executeQuery('UPDATE questionnaire_metric SET ? WHERE id = ?', [data, questionnaire_metric_id]);
+    return queryService.executeQuery('UPDATE questionnaire_metric SET ? WHERE id = ?', [data, questionnaire_metric_id]);
   };
 
   this.rollupMetricsToDonor = function(status, questionnaire_id){
-    console.log(questionnaire_id);
-        return queryService.executeQuery('UPDATE questionnaire_metric SET status = ? WHERE questionnaire_id = ?', [status, questionnaire_id]);
+    return queryService.executeQuery('UPDATE questionnaire_metric SET status = ? WHERE questionnaire_id = ?', [status, questionnaire_id]);
   };
 
   this.updateQuestionnaireStatus = function(questionnaire_id, status){
