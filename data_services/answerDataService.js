7fffd6e00cb77ca7374a58701bc47331c8aa0431
denver <denver@projectcodex.co>
2016-03-11 11:26:13 +0200

    able to update status to answered for facilitator to see

--- data_services/answerDataService.js
+++ data_services/answerDataService.js
@@ -1,8 +1,17 @@
 const QueryService = require('../data_services/query-service');
 
 module.exports = function (connection) {
-const queryService = new QueryService(connection);
-this.answeredMetrics = function(data,questionnaire_metric_id){
+  const queryService = new QueryService(connection);
+
+  this.answeredMetrics = function(data,questionnaire_metric_id){
        return queryService.executeQuery('UPDATE questionnaire_metric SET ? WHERE id = ?', [data, questionnaire_metric_id]);
-   };
+  };
+
+  this.rollupMetricsToDonor = function(questionnaire_metric_id){
+        return queryService.executeQuery('UPDATE questionnaire_metric SET status = \'released\' WHERE id = ?', questionnaire_metric_id);
+  };
+
+  this.updateQuestionnaireStatus = function(questionnaire_id, status){
+    return queryService.executeQuery('UPDATE questionnaire_metric SET status = ? WHERE questionnaire_id = ?', [status, questionnaire_id]);
+  };
 };
