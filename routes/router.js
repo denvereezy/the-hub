f86eda2fbe1feb524c1642cfacb30698c99413b2
denver <denver@projectcodex.co>
2016-03-13 07:50:48 +0200

    route removed

--- routes/router.js
+++ routes/router.js
@@ -6,13 +6,13 @@ exports.login = function (req, res) {
   res.render('login',{layout:false});
 };
 
-exports.dashboard = function  (req, res)  {
-  var donor = req.session.type === 'Donor';
-  var facilitator = req.session.type === 'Facilitator';
-  var startup = req.session.type === 'Startup';
-  res.render('dashboard',{user:req.session.user,
-  entity:req.session.entity, donor:donor, facilitator:facilitator, startup:startup});
-};
+// exports.dashboard = function  (req, res)  {
+//   var donor = req.session.type === 'Donor';
+//   var facilitator = req.session.type === 'Facilitator';
+//   var startup = req.session.type === 'Startup';
+//   res.render('dashboard',{user:req.session.user,
+//   entity:req.session.entity, donor:donor, facilitator:facilitator, startup:startup});
+// };
 
 exports.questionnaire = function  (req, res)  {
   res.render('setup-questionnaire-step-1',{user:req.session.user,
