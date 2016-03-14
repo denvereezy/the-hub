3e07f78a706b2cd4b1d854422eec0c066f509c97
denver <denver@projectcodex.co>
2016-03-14 08:46:52 +0200

    console removed

--- routes/viewQuestionnaire.js
+++ routes/viewQuestionnaire.js
@@ -8,40 +8,17 @@ exports.show = function (req, res, next) {
            const startup = req.session.type === "Startup";
            const viewQuestionnnaireDataService = services.viewQuestionnnaireDataService;
            var id = req.session.entity_id;
-           viewQuestionnnaireDataService.showQuestionnaires(id)
-               .then(function(questionnaire){
-                   res.render('dashboard', {
-                       questionnaire : questionnaire,
-                       facilitatorQuestionnaire:questionnaire,
-                       user:req.session.user,
-                       entity:req.session.entity,
-                       donor:donor,
-                       facilitator:facilitator,
-                       startup:startup
-                   });
-               });
-       })
-       .catch(function(err){
-           next(err);
-       });
-};
-
-exports.showResults = function (req, res, next) {
-   req.getServices()
-       .then(function(services){
-           const donor = req.session.type === 'Donor';
-           const facilitator = req.session.type === 'Facilitator';
-           const viewQuestionnnaireDataService = services.viewQuestionnnaireDataService;
-           var id = req.session.entity_id;
            Promise.join(viewQuestionnnaireDataService.showQuestionnaires(id),viewQuestionnnaireDataService.showCreatedQuestionnaires(id),
                function(questionnaire, donorQuestionnaires){
                    res.render('dashboard', {
                        facilitatorQuestionnaire:questionnaire,
                        donorQuestionnaires:donorQuestionnaires,
+                       questionnaire : questionnaire,
                        user:req.session.user,
                        entity:req.session.entity,
+                       donor:donor,
                        facilitator:facilitator,
-                       donor:donor
+                       startup:startup
                    });
                });
        })
