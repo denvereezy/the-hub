9a122b0b5bc7912c3dad99272e40de60b6e2f9ee
denver <denver@projectcodex.co>
2016-03-14 11:08:04 +0200

    stop sign up page from hanging when adding new user

--- data_services/signupDataService.js
+++ data_services/signupDataService.js
@@ -2,27 +2,31 @@ const QueryService = require("../data_services/query-service");
 const Promise = require('bluebird');
 const bcrypt = require('bcrypt');
 
-module.exports = function(connection){
+var encryptPassword = function(password) {
+  return new Promise(function(resolve, reject) {
+    bcrypt.genSalt(10, function(err, salt) {
+      bcrypt.hash(password, salt, function(err, hash) {
+        if (err) {
+          return reject(err);
+        }
+        resolve(hash)
+      });
+    });
+  });
+};
+
+module.exports = function(connection) {
   const queryService = new QueryService(connection);
 
-    this.addEntity = function(entity){
-      return queryService.executeQuery('insert into entity set ?', entity);
-    };
+  this.addEntity = function(entity) {
+    return queryService.executeQuery('insert into entity set ?', entity);
+  };
 
-    this.addUser = function(user){
-        return new Promise(function(resolve,reject){
-            bcrypt.genSalt(10, function(err, salt) {
-                bcrypt.hash(user.password, salt, function(err, hash) {
-                    if (err){
-                        return reject (err);
-                    }
-                    user.password = hash;
-                    queryService.executeQuery('insert into user set ?', user, function(err, results) {
-                        if (err) return reject (err);
-                        resolve(results);
-                    });
-                });
-            });
-        });
-    };
+  this.addUser = function(user) {
+    return encryptPassword(user.password)
+      .then(function(encryptedPassword) {
+        user.password = encryptedPassword;
+        return queryService.executeQuery('insert into user set ?', user);
+      });
+  };
 };
