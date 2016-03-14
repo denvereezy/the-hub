71fbc9b09a1843b1a3323050133074e15a3ec255
denver <denver@projectcodex.co>
2016-03-14 11:08:34 +0200

    stop sign up page from hanging when adding new user

--- routes/signup.js
+++ routes/signup.js
@@ -1,34 +1,32 @@
-exports.add = function (req, res, next) {
-    req.getServices()
-      .then(function(services){
-        var entity = {
-            name  : req.body.name,
-            address : req.body.address,
-            type : req.body.type
-        };
+exports.add = function(req, res, next) {
+  req.getServices()
+    .then(function(services) {
+      var entity = {
+        name: req.body.name,
+        address: req.body.address,
+        type: req.body.type
+      };
 
-        var user = {
-            email :  req.body.email,
-            role  :  'admin',
-            status  : 'created',
-            password  : 'password',
-            firstName : req.body.firstName,
-            lastName  : req.body.lastName
-        };
+      var user = {
+        email: req.body.email,
+        role: 'admin',
+        status: 'created',
+        password: 'password',
+        firstName: req.body.firstName,
+        lastName: req.body.lastName
+      };
 
-        const signupDataService = services.signupDataService;
-        signupDataService.addEntity(entity)
-          .then(function(data){
-            user.entity_id = data.insertId;
-            return signupDataService.addUser(user);
-          })
-            .then(function(user){
-            })
-              .then(function(results){
-                  res.redirect('/');
-              })
-                .catch(function(err){
-                  next(err);
-                });
+      const signupDataService = services.signupDataService;
+      signupDataService.addEntity(entity)
+        .then(function(data) {
+          user.entity_id = data.insertId;
+          return signupDataService.addUser(user);
+        })
+      .then(function(results) {
+          return res.redirect('/');
+        })
+        .catch(function(err) {
+          next(err);
         });
+    });
 };
