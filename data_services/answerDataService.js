0e683b35bdf3340b67ee060b18f508ae32e64d3d
denver <denver@projectcodex.co>
2016-03-15 20:54:22 +0200

    code cleaned up

--- data_services/answerDataService.js
+++ data_services/answerDataService.js
@@ -1,17 +1,17 @@
 const QueryService = require('../data_services/query-service');
 
-module.exports = function (connection) {
+module.exports = function(connection) {
   const queryService = new QueryService(connection);
 
-  this.answeredMetrics = function(data,questionnaire_metric_id){
+  this.answeredMetrics = function(data, questionnaire_metric_id) {
     return queryService.executeQuery('UPDATE questionnaire_metric SET ? WHERE id = ?', [data, questionnaire_metric_id]);
   };
 
-  this.rollupMetricsToDonor = function(status, questionnaire_id){
+  this.rollupMetricsToDonor = function(status, questionnaire_id) {
     return queryService.executeQuery('UPDATE questionnaire_metric SET status = ? WHERE questionnaire_id = ?', [status, questionnaire_id]);
   };
 
-  this.updateQuestionnaireStatus = function(status, questionnaire_id){
+  this.updateQuestionnaireStatus = function(status, questionnaire_id) {
     return queryService.executeQuery('UPDATE questionnaire_metric SET status = ? WHERE questionnaire_id = ?', [status, questionnaire_id]);
   };
 };
