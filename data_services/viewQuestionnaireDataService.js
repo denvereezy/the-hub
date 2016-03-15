966ecd33f9aa0ff634e2c7584ef740bcdefd5d7f
denver <denver@projectcodex.co>
2016-03-15 20:58:02 +0200

    code cleaned up

--- data_services/viewQuestionnaireDataService.js
+++ data_services/viewQuestionnaireDataService.js
@@ -1,12 +1,12 @@
 const QueryService = require("../data_services/query-service");
 
 module.exports = function(connection) {
-    const queryService = new QueryService(connection);
-    this.showQuestionnaires = function(id) {
-      return queryService.executeQuery('SELECT id, entity_id, name,base_questionnaire_id, DATE_FORMAT(dueDate,"%d %b %y") as dueDate FROM questionnaire WHERE parent_questionnaire_id != \'null\' and entity_id = ? order by id desc', id);
-    };
+  const queryService = new QueryService(connection);
+  this.showQuestionnaires = function(id) {
+    return queryService.executeQuery('SELECT id, entity_id, name,base_questionnaire_id, DATE_FORMAT(dueDate,"%d %b %y") as dueDate FROM questionnaire WHERE parent_questionnaire_id != \'null\' and entity_id = ? order by id desc', id);
+  };
 
-    this.showCreatedQuestionnaires = function(id) {
-      return queryService.executeQuery('SELECT id, entity_id, name, DATE_FORMAT(dueDate,"%d %b %y") as dueDate,base_questionnaire_id FROM questionnaire WHERE entity_id = ? order by id desc', id);
-    };
+  this.showCreatedQuestionnaires = function(id) {
+    return queryService.executeQuery('SELECT id, entity_id, name, DATE_FORMAT(dueDate,"%d %b %y") as dueDate,base_questionnaire_id FROM questionnaire WHERE entity_id = ? order by id desc', id);
+  };
 };
