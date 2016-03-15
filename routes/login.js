8cebcb4a37c4c1f53e8490a1e68bf791ec4378a5
denver <denver@projectcodex.co>
2016-03-15 10:38:33 +0200

    fixed app crash on invalid user login

--- routes/login.js
+++ routes/login.js
@@ -1,38 +1,41 @@
 const bcrypt = require('bcrypt');
 
-exports.userCheck = function (req, res, next) {
-        if (req.session.user){
-             next();
-        }
-        else{
-             res.redirect("/")
-        }
+exports.userCheck = function(req, res, next) {
+  if (req.session.user) {
+    next();
+  } else {
+    res.redirect("/")
+  }
 };
 
 exports.userLogin = function(req, res, next) {
-    req.getServices()
-    .then(function(services){
+  req.getServices()
+    .then(function(services) {
       var email = req.body.email;
       const loginDataService = services.loginDataService;
       loginDataService.login(email)
-      .then(function(results){
+        .then(function(results) {
           var user = results[0];
-          bcrypt.compare(req.body.password, user.password,function(err, pass) {
-              if (pass) {
-                req.session.user = user.firstName;
-                req.session.role =  user.role;
-                req.session.entity_id = user.entity_id;
-                req.session.entity = user.name;
-                req.session.type = user.type;
-                return res.redirect("/dashboard")
-              }
-              else {
-                return res.redirect('/');
-              };
+
+          if(user === undefined){
+            return res.redirect('/');
+          };
+          
+          bcrypt.compare(req.body.password, user.password, function(err, pass) {
+            if (pass) {
+              req.session.user = user.firstName;
+              req.session.role = user.role;
+              req.session.entity_id = user.entity_id;
+              req.session.entity = user.name;
+              req.session.type = user.type;
+              return res.redirect("/dashboard")
+            } else {
+              return res.redirect('/');
+            };
           });
-      })
-  })
-    .catch(function(err){
+        })
+    })
+    .catch(function(err) {
       next(err);
     });
 };
