25e252d3049d80d7a74d6832d23a7ba9c4c96099
denver <denver@projectcodex.co>
2016-03-17 14:39:12 +0200

    update & edit functions added

--- data_services/questionDataService.js
+++ data_services/questionDataService.js
@@ -6,4 +6,16 @@ module.exports = function(connection) {
   this.showAll = function(questionnaire_id) {
     return queryService.executeQuery('SELECT questionnaire_metric.id as questionnaire_metric_id, metric_id, questionnaire_id,base_questionnaire_id, title, description, value from questionnaire_metric inner join questionnaire on questionnaire_metric.questionnaire_id = questionnaire.id inner join metric on metric_id = metric.id where questionnaire_id = ?', questionnaire_id);
   };
+
+  this.entityMetrics = function(entity_id) {
+    return queryService.executeQuery('select * from metric where entity_id = ?', entity_id);
+  };
+
+  this.editMetric = function(id) {
+    return queryService.executeQuery('SELECT * FROM metric WHERE id = ?', id);
+  };
+
+  this.updateMetric = function(data, id) {
+    return queryService.executeQuery('UPDATE metric SET ? WHERE id = ?', [data, id]);
+  };
 };
