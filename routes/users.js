e856179d8217df900182af63a064e403c02e90c4
denver <denver@projectcodex.co>
2016-03-20 11:03:10 +0200

    users add function added

--- routes/users.js
+++ routes/users.js
@@ -14,3 +14,24 @@ exports.showUsers = function(req, res, next) {
       next(err);
     });
 };
+
+exports.addUser = function(req, res, next) {
+  req.getServices()
+    .then(function(connection) {
+      const data = {
+        email: req.body.email,
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        password: 'password',
+        entity_id: req.session.entity_id
+      };
+      const userDataService = services.userDataService;
+      userDataService.addUserToEntity(data)
+        .then(function(results) {
+          res.redirect('/users');
+        })
+    })
+    .catch(function(error) {
+      next(error);
+    });
+};
