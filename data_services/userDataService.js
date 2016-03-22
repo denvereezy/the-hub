ae543031b108b34af09b4b015ae6208fdd0bd5eb
denver <denver@projectcodex.co>
2016-03-22 09:04:45 +0200

    able to delete user

--- data_services/userDataService.js
+++ data_services/userDataService.js
@@ -6,4 +6,8 @@ module.exports = function (connection) {
   this.showEntityUsers = function (entity_id) {
     return queryService.executeQuery('select * from user where entity_id = ?', entity_id);
   };
+
+  this.deleteUser = function (id) {
+    return queryService.executeQuery('delete from user where id = ?', id);
+  };
 };
