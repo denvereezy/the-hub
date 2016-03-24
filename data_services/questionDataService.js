6104a52dfbb65818898d41afd61ede73bc3057b9
denver <denver@projectcodex.co>
2016-03-24 14:53:59 +0200

    status added to querie

--- data_services/questionDataService.js
+++ data_services/questionDataService.js
@@ -4,7 +4,7 @@ module.exports = function(connection) {
   const queryService = new QueryService(connection);
 
   this.showAll = function(questionnaire_id) {
-    return queryService.executeQuery('SELECT questionnaire_metric.id as questionnaire_metric_id, metric_id, questionnaire_id,base_questionnaire_id, title, description, value from questionnaire_metric inner join questionnaire on questionnaire_metric.questionnaire_id = questionnaire.id inner join metric on metric_id = metric.id where questionnaire_id = ?', questionnaire_id);
+    return queryService.executeQuery('SELECT status, questionnaire_metric.id as questionnaire_metric_id, metric_id, questionnaire_id,base_questionnaire_id, title, description, value from questionnaire_metric inner join questionnaire on questionnaire_metric.questionnaire_id = questionnaire.id inner join metric on metric_id = metric.id where questionnaire_id = ?', questionnaire_id);
   };
 
   this.entityMetrics = function(entity_id) {
