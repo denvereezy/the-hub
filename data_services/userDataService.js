d0ee5348ad2813b89f9857dcc985ab935354335d
denver <denver@projectcodex.co>
2016-03-30 13:50:25 +0200

    error from query sesolved

--- data_services/userDataService.js
+++ data_services/userDataService.js
@@ -1,13 +1,37 @@
 const QueryService = require('../data_services/query-service');
+const Promise = require('bluebird');
+const bcrypt = require('bcrypt');
 
-module.exports = function (connection) {
+var encryptPassword = function(password) {
+  return new Promise(function(resolve, reject) {
+    bcrypt.genSalt(10, function(err, salt) {
+      bcrypt.hash(password, salt, function(err, hash) {
+        if (err) {
+          return reject(err);
+        }
+        resolve(hash)
+      });
+    });
+  });
+};
+
+module.exports = function(connection) {
   const queryService = new QueryService(connection);
 
-  this.showEntityUsers = function (entity_id, user) {
+  this.showEntityUsers = function(entity_id, user) {
     return queryService.executeQuery('select * from user where entity_id = ? and firstName != ?', [entity_id, user]);
   };
 
-  this.deleteUser = function (id) {
+  this.deleteUser = function(id) {
     return queryService.executeQuery('delete from user where id = ?', id);
   };
+
+  this.confirmAccount = function(user, token) {
+    return encryptPassword(user.password)
+      .then(function(encryptedPassword) {
+        user.password = encryptedPassword;
+        return queryService.executeQuery('update user set ? where token = ?', [user, token]);
+      });
+  };
+
 };
