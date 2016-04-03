cbc5f7c9a117d6f65912d1e433c4621ea92dd20b
denver <denver@projectcodex.co>
2016-04-03 21:49:37 +0200

    query to allow facilitator to add questions to questionnaire

--- data_services/questionDataService.js
+++ data_services/questionDataService.js
@@ -8,7 +8,7 @@ module.exports = function(connection) {
   };
 
   this.entityMetrics = function(entity_id) {
-    return queryService.executeQuery('select * from metric where entity_id = ?', entity_id);
+    return queryService.executeQuery('SELECT * from metric where entity_id = ?', entity_id);
   };
 
   this.editMetric = function(id) {
@@ -17,5 +17,9 @@ module.exports = function(connection) {
 
   this.updateMetric = function(data, id) {
     return queryService.executeQuery('UPDATE metric SET ? WHERE id = ?', [data, id]);
+  };
+
+  this.showFacilitatorCrearedQuestions = function(questionnaire_id, entity_id) {
+    return queryService.executeQuery('SELECT status, questionnaire_metric.id as questionnaire_metric_id, metric_id, questionnaire_id,base_questionnaire_id, title, description, value from questionnaire_metric inner join questionnaire on questionnaire_metric.questionnaire_id = questionnaire.id inner join metric on metric_id = metric.id where questionnaire_id = ? and entity_id = ?', [questionnaire_id, entity_id]);
   };
 };
