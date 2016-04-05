2f08dcac9087efe541b0010227075619a40015cf
denver <denver@projectcodex.co>
2016-04-05 15:29:00 +0200

    new route added

--- app.js
+++ app.js
@@ -85,6 +85,7 @@ app.get('/questionnaire/questions/view/:id', login.userCheck, questions.show);
 app.get('/questionnaire/:id/questions/:base_questionnaire_id/results', login.userCheck, questions.metricResults);
 app.get('/questionnaire/questions/:questionnaire_id', login.userCheck, answerQuestionnaire.showQuestions);
 app.post('/questionnaire/questions/view/:questionnaire_id', login.userCheck, setupQuestionnaire.linkMetricToQuestionnaire);
+app.post('/questionnaire/facilitatorQuestions/link/:questionnaire_id', login.userCheck, questions.linkFacilitatorMetricsToQuestionnaire);
 app.post('/questionnaire/:questionnaire_id/answer/:questionnaire_metric_id', login.userCheck, answerQuestionnaire.answers);
 app.post('/questionnaire/:base_questionnaire_id/rolledup-answers',login.userCheck, answerQuestionnaire.releaseAnswersToDonor);
 app.post('/questionnaire/submit/answered/:questionnaire_id', login.userCheck, answerQuestionnaire.answeredQuestionnaire);
