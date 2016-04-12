cba1578003f1efda1aa9ea502ccff590cc4ef7bc
denver <denver@projectcodex.co>
2016-04-12 08:36:20 +0200

    checking if email is in use when adding a new user

--- routes/users.js
+++ routes/users.js
@@ -2,112 +2,124 @@ const nodemailer = require('nodemailer');
 const uuid = require('node-uuid');
 
 exports.showUsers = function(req, res, next) {
-  req.getServices()
-    .then(function(services) {
-      const entity_id = req.session.entity_id;
-      const user = req.session.user;
-      const entity = req.session.entity;
-      const userDataService = services.userDataService;
-      userDataService.showEntityUsers(entity_id, user)
-        .then(function(users) {
-          res.render('users', {
-            users: users,
-            user: user,
-            entity: entity
-          })
+    req.getServices()
+        .then(function(services) {
+            const entity_id = req.session.entity_id;
+            const user = req.session.email;
+            const entity = req.session.entity;
+            const userDataService = services.userDataService;
+            userDataService.showEntityUsers(entity_id, user)
+                .then(function(users) {
+                    res.render('users', {
+                        users: users,
+                        user: user,
+                        entity: entity
+                    })
+                })
         })
-    })
-    .catch(function(err) {
-      next(err);
-    });
+        .catch(function(err) {
+            next(err);
+        });
 };
 
 exports.addUser = function(req, res, next) {
-  req.getServices()
-    .then(function(services) {
-      const data = {
-        email: req.body.email,
-        firstName: req.body.firstName,
-        lastName: req.body.lastName,
-        password: '',
-        entity_id: req.session.entity_id,
-        role: 'admin',
-        status: 'invited',
-        token: uuid.v4()
-      };
+    req.getServices()
+        .then(function(services) {
+            var email = req.body.email;
+            const checkEmailAvailablity = services.loginDataService;
+            checkEmailAvailablity.login(email)
+                .then(function(results) {
+                    var user = results[0];
+                    if (user !== undefined) {
+                        req.flash('alert', 'Email entered is in use. Please try again');
+                        res.redirect('/users');
+                    } else {
 
-      var mailOpts, smtpConfig;
+                        const data = {
+                            email: req.body.email,
+                            firstName: req.body.firstName,
+                            lastName: req.body.lastName,
+                            password: '',
+                            entity_id: req.session.entity_id,
+                            role: 'admin',
+                            status: 'invited',
+                            token: uuid.v4()
+                        };
 
-      smtpConfig = nodemailer.createTransport('SMTP', {
-        service: 'Gmail',
-        auth: {
-          user: 'APP EMAIL',
-          pass: 'APP PASSWORD'
-        }
-      });
+                        var mailOpts, smtpConfig;
 
-      mailOpts = {
-        from: req.session.entity,
-        to: data.email,
-        subject: 'invite to join',
-        text: data.firstName + ' you have been invited by ' + req.session.entity + ' to become a user. Please follow the link to setup your password. Your current email address is used to login.' + 'http://hub.projectcodex.co/account/verifyaccount/' + data.token
-      };
+                        smtpConfig = nodemailer.createTransport('SMTP', {
+                            service: 'Gmail',
+                            auth: {
+                                user: 'APP EMAIL',
+                                pass: 'APP PASSWORD'
+                            }
+                        });
 
-      const signupDataService = services.signupDataService;
-      signupDataService.addUser(data)
-        .then(function(results) {
-          smtpConfig.sendMail(mailOpts);
-          req.flash('success', 'user added successfully');
-          res.redirect('/users');
-        })
-    .catch(function(error) {
-      next(error);
-    });
-  });
+                        mailOpts = {
+                            from: req.session.entity,
+                            to: data.email,
+                            subject: 'invite to join',
+                            text: data.firstName + ' you have been invited by ' + req.session.entity + ' to become a user. Please follow the link to setup your password. Your current email address is used to login.' + 'http://hub.projectcodex.co/account/verifyaccount/' + data.token
+                        };
+
+                        const signupDataService = services.signupDataService;
+                        signupDataService.addUser(data)
+                            .then(function(results) {
+                                smtpConfig.sendMail(mailOpts);
+                                req.flash('success', 'user added successfully');
+                                res.redirect('/users');
+                            })
+                            .catch(function(error) {
+                                next(error);
+                            });
+                    };
+                });
+        });
 };
 
 exports.confirmUser = function(req, res, next) {
-  req.getServices()
-    .then(function(services) {
-      const token = req.params.token;
-      const password = req.body.password;
-      const confirmPassword = req.body.password2;
-      const user = {
-        token: null,
-        status: 'active'
-      };
+    req.getServices()
+        .then(function(services) {
+            const token = req.params.token;
+            const password = req.body.password;
+            const confirmPassword = req.body.password2;
+            const user = {
+                token: null,
+                status: 'active'
+            };
 
-      if (confirmPassword === password) {
-        user.password = confirmPassword;
-      } else {
-        req.flash('alert', 'Passwords does not match, please try again');
-        res.redirect('/account/verifyaccount/' + token)
-      }
+            if (confirmPassword === password) {
+                user.password = confirmPassword;
+            } else {
+                req.flash('alert', 'Passwords does not match, please try again');
+                res.redirect('/account/verifyaccount/' + token)
+            }
 
-      const userDataService = services.userDataService;
-      userDataService.confirmAccount(user, token)
-        .then(function(results) {
-          req.flash('success', 'Password reset was successful');
-          res.redirect('/');
+            const userDataService = services.userDataService;
+            userDataService.confirmAccount(user, token)
+                .then(function(results) {
+                    req.flash('success', 'Password reset was successful');
+                    res.redirect('/');
+                })
         })
-    })
-    .catch(function(error) {
-      next(error);
-    });
+        .catch(function(error) {
+            next(error);
+        });
 };
 
 exports.delete = function(req, res, next) {
-  req.getServices()
-    .then(function(services) {
-      const id = req.params.id;
-      const userDataService = services.userDataService;
-      userDataService.deleteUser(id)
-        .then(function(results) {
-          req.flash('success', 'user removed');
-          res.redirect('/users');
+    req.getServices()
+        .then(function(services) {
+            const id = req.params.id;
+            const userDataService = services.userDataService;
+            userDataService.deleteUser(id)
+                .then(function(results) {
+                    req.flash('success', 'user removed');
+                    res.redirect('/users');
+                })
         })
-    })
-    .catch(function(error) {
-      next(error);
-    });
+        .catch(function(error) {
+            next(error);
+        });
 };
