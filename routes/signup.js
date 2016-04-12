7693c8b2d21a9f75b1cbb6e14159c370e4b3713f
denver <denver@projectcodex.co>
2016-04-12 08:44:03 +0200

    checking if email is in use when adding a new user

--- routes/signup.js
+++ routes/signup.js
@@ -1,33 +1,44 @@
 exports.add = function(req, res, next) {
-  req.getServices()
-    .then(function(services) {
-      var entity = {
-        name: req.body.name,
-        address: req.body.address,
-        type: req.body.type
-      };
+    req.getServices()
+        .then(function(services) {
+            var email = req.body.email;
+            const checkEmailAvailablity = services.loginDataService;
+            checkEmailAvailablity.login(email)
+                .then(function(results) {
+                    var user = results[0];
+                    if (user !== undefined) {
+                        req.flash('alert', 'Email entered is in use. Please try again');
+                        res.redirect('/signup');
+                    } else {
+                        var entity = {
+                            name: req.body.name,
+                            address: req.body.address,
+                            type: req.body.type
+                        };
 
-      var user = {
-        email: req.body.email,
-        role: 'admin',
-        status: 'active',
-        password: req.body.password,
-        firstName: req.body.firstName,
-        lastName: req.body.lastName
-      };
+                        var user = {
+                            email: req.body.email,
+                            role: 'admin',
+                            status: 'active',
+                            password: req.body.password,
+                            firstName: req.body.firstName,
+                            lastName: req.body.lastName
+                        };
 
-      const signupDataService = services.signupDataService;
-      signupDataService.addEntity(entity)
-        .then(function(data) {
-          user.entity_id = data.insertId;
-          return signupDataService.addUser(user);
-        })
-        .then(function(results) {
-          req.flash('success', 'Sign up was successful');
-          return res.redirect('/');
-        })
-        .catch(function(err) {
-          next(err);
+                        const signupDataService = services.signupDataService;
+                        signupDataService.addEntity(entity)
+                            .then(function(data) {
+                                user.entity_id = data.insertId;
+                                return signupDataService.addUser(user);
+                            })
+                            .then(function(results) {
+                                req.flash('success', 'Sign up was successful');
+                                return res.redirect('/');
+                            })
+                            .catch(function(err) {
+                                next(err);
+                            });
+                    };
+                });
         });
-    });
 };
