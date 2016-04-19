42f93feaf217609e09c4a52de5a08b0670721f3f
denver daniels <denver@projectcodex.co>
2016-04-19 15:28:45 +0200

    query working

--- data_services/superUserDataService.js
+++ data_services/superUserDataService.js
@@ -1,6 +1,9 @@
 const QueryService = require('../data_services/query-service');
 
 module.exports = function(connection) {
-  const queryService = new QueryService(connection);
+    const queryService = new QueryService(connection);
 
-}; 
+    this.showRequests = function() {
+        return queryService.executeQuery('select firstname, email, name from user inner join entity on user.entity_id = entity.id where status = \'pending\'');
+    };
+};
