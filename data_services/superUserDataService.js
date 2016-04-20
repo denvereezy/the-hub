557e1c17849c98b6b92e13eecf191a695e2025b8
denver daniels <denver@projectcodex.co>
2016-04-20 15:03:40 +0200

    queries completed

--- data_services/superUserDataService.js
+++ data_services/superUserDataService.js
@@ -4,14 +4,14 @@ module.exports = function(connection) {
     const queryService = new QueryService(connection);
 
     this.showRequests = function() {
-        return queryService.executeQuery('select firstname, email, name from user inner join entity on user.entity_id = entity.id where status = \'pending\'');
+        return queryService.executeQuery('select user.id as user_id, firstname, email, name from user inner join entity on user.entity_id = entity.id where status = \'pending\'');
     };
 
     this.acceptRequest = function(id) {
-        return queryService.executeQuery('update user');
+        return queryService.executeQuery('update user set status = \'active\' where id = ?', id);
     };
 
     this.rejectRequest = function(id) {
-        return queryService.executeQuery('delete from');
+        return queryService.executeQuery('update user set status = \'rejected\' where id = ?', id);
     };
 };
