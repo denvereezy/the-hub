778479ef957712d192d65e9c7fd7dda92ff32b61
denver daniels <denver@projectcodex.co>
2016-04-20 13:13:08 +0200

    beautified

--- routes/login.js
+++ routes/login.js
@@ -1,46 +1,46 @@
 const bcrypt = require('bcrypt');
 
 exports.userCheck = function(req, res, next) {
-  if (req.session.user) {
-    next();
-  } else {
-    res.redirect("/")
-  }
+    if (req.session.user) {
+        next();
+    } else {
+        res.redirect("/")
+    }
 };
 
 exports.userLogin = function(req, res, next) {
-  req.getServices()
-    .then(function(services) {
-      var email = req.body.email;
-      const loginDataService = services.loginDataService;
-      loginDataService.login(email)
-        .then(function(results) {
-          var user = results[0];
-          if (user === undefined) {
-            req.flash('alert', 'Email or Password entered is Invalid, please try again');
-            return res.redirect("/");
-          } else if (user.status === 'invited') {
-            req.flash('alert', 'Email or Password entered is Invalid, please try again');
-            return res.redirect("/");
-          };
+    req.getServices()
+        .then(function(services) {
+            var email = req.body.email;
+            const loginDataService = services.loginDataService;
+            loginDataService.login(email)
+                .then(function(results) {
+                    var user = results[0];
+                    if (user === undefined) {
+                        req.flash('alert', 'Email or Password entered is Invalid, please try again');
+                        return res.redirect("/");
+                    } else if (user.status === 'invited') {
+                        req.flash('alert', 'Email or Password entered is Invalid, please try again');
+                        return res.redirect("/");
+                    };
 
-          bcrypt.compare(req.body.password, user.password, function(err, pass) {
-            if (pass) {
-              req.session.user = user.firstName;
-              req.session.email = user.email;
-              req.session.role = user.role;
-              req.session.entity_id = user.entity_id;
-              req.session.entity = user.name;
-              req.session.type = user.type;
-              return res.redirect("/dashboard")
-            } else {
-              req.flash('alert', 'Email or Password entered is Invalid, please try again');
-              return res.redirect("/")
-            };
-          })
+                    bcrypt.compare(req.body.password, user.password, function(err, pass) {
+                        if (pass) {
+                            req.session.user = user.firstName;
+                            req.session.email = user.email;
+                            req.session.role = user.role;
+                            req.session.entity_id = user.entity_id;
+                            req.session.entity = user.name;
+                            req.session.type = user.type;
+                            return res.redirect("/dashboard")
+                        } else {
+                            req.flash('alert', 'Email or Password entered is Invalid, please try again');
+                            return res.redirect("/")
+                        };
+                    })
+                })
         })
-    })
-    .catch(function(err) {
-      next(err);
-    });
+        .catch(function(err) {
+            next(err);
+        });
 };
