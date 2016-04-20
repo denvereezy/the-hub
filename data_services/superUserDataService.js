45db6366f2a6f3e8395b7229d667139b9b7664ef
denver daniels <denver@projectcodex.co>
2016-04-20 08:44:28 +0200

    functions done just needs query

--- data_services/superUserDataService.js
+++ data_services/superUserDataService.js
@@ -6,4 +6,12 @@ module.exports = function(connection) {
     this.showRequests = function() {
         return queryService.executeQuery('select firstname, email, name from user inner join entity on user.entity_id = entity.id where status = \'pending\'');
     };
+
+    this.acceptRequest = function(id) {
+        return queryService.executeQuery('update user');
+    };
+
+    this.rejectRequest = function(id) {
+        return queryService.executeQuery('delete from');
+    };
 };
