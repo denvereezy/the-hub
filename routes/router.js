e769f777e2383c185cb6c5a92ccec62c900e4e06
denver daniels <denver@projectcodex.co>
2016-04-20 13:14:26 +0200

    beautified

--- routes/router.js
+++ routes/router.js
@@ -1,32 +1,32 @@
 exports.signup = function(req, res) {
-  res.render('signup', {
-    layout: false
-  });
+    res.render('signup', {
+        layout: false
+    });
 };
 
 exports.login = function(req, res) {
-  res.render('login', {
-    layout: false
-  });
+    res.render('login', {
+        layout: false
+    });
 };
 
 exports.questionnaire = function(req, res) {
-  res.render('setup-questionnaire-step-1', {
-    user: req.session.user,
-    entity: req.session.entity
-  });
+    res.render('setup-questionnaire-step-1', {
+        user: req.session.user,
+        entity: req.session.entity
+    });
 };
 
 exports.logout = function(req, res) {
-  delete req.session.user
-  res.redirect("/");
+    delete req.session.user
+    res.redirect("/");
 };
 
 exports.verifyaccount = function(req, res) {
-res.render('verifyaccount',{
-  user: req.session.user,
-  entity: req.session.entity,
-  layout: false,
-  token:req.params.token
-});
+    res.render('verifyaccount', {
+        user: req.session.user,
+        entity: req.session.entity,
+        layout: false,
+        token: req.params.token
+    });
 };
