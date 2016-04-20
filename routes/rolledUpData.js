f1bce5e62a4b80fb55a8598f3cffeba7826f8e5e
denver daniels <denver@projectcodex.co>
2016-04-20 13:13:52 +0200

    beautified

--- routes/rolledUpData.js
+++ routes/rolledUpData.js
@@ -1,17 +1,17 @@
 exports.showResults = function(req, res, next) {
-  req.getServices()
-    .then(function(services) {
-      const rollupDataService = services.rollupDataService;
-      rollupDataService.rollupMetrics()
-        .then(function(values) {
-          res.render('questionnaire-results', {
-            values: values,
-            user: req.session.user,
-            entity: req.session.entity
-          });
+    req.getServices()
+        .then(function(services) {
+            const rollupDataService = services.rollupDataService;
+            rollupDataService.rollupMetrics()
+                .then(function(values) {
+                    res.render('questionnaire-results', {
+                        values: values,
+                        user: req.session.user,
+                        entity: req.session.entity
+                    });
+                });
+        })
+        .catch(function(err) {
+            next(err);
         });
-    })
-    .catch(function(err) {
-      next(err);
-    });
 };
