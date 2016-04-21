a14c526fc6250f895074143c5a43f4a1100da6ad
denver daniels <denver@projectcodex.co>
2016-04-21 09:58:06 +0200

    middleware added

--- routes/login.js
+++ routes/login.js
@@ -22,6 +22,9 @@ exports.userLogin = function(req, res, next) {
                     } else if (user.status === 'invited') {
                         req.flash('alert', 'Email or Password entered is Invalid, please try again');
                         return res.redirect("/");
+                    }else if (user.status === 'rejected') {
+                        req.flash('alert', 'Email or Password entered is Invalid, please try again');
+                        return res.redirect("/");
                     };
 
                     bcrypt.compare(req.body.password, user.password, function(err, pass) {
