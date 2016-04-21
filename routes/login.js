dfe555e307fe7d0e065bc16ae62f6e9e90e115e3
denver daniels <denver@projectcodex.co>
2016-04-21 11:01:37 +0200

    middleware for super user added

--- routes/login.js
+++ routes/login.js
@@ -8,6 +8,14 @@ exports.userCheck = function(req, res, next) {
     }
 };
 
+exports.checkSuperUser = function(req, res, next) {
+    if (req.session.role === 'Super User') {
+        next();
+    } else {
+        res.redirect("/")
+    }
+};
+
 exports.userLogin = function(req, res, next) {
     req.getServices()
         .then(function(services) {
@@ -22,25 +30,38 @@ exports.userLogin = function(req, res, next) {
                     } else if (user.status === 'invited') {
                         req.flash('alert', 'Email or Password entered is Invalid, please try again');
                         return res.redirect("/");
-                    }else if (user.status === 'rejected') {
+                    } else if (user.status === 'rejected') {
                         req.flash('alert', 'Email or Password entered is Invalid, please try again');
                         return res.redirect("/");
-                    };
+                    } else if (user.role === 'Super User') {
+                        bcrypt.compare(req.body.password, user.password, function(err, pass) {
+                            if (pass) {
+                                req.session.user = user.firstName;
+                                req.session.email = user.email;
+                                req.session.role = user.role;
+                                req.session.entity_id = user.entity_id;
+                                req.session.entity = user.name;
+                                req.session.type = user.type;
+                                return res.redirect("/root")
+                            };
+                        });
+                    } else {
 
-                    bcrypt.compare(req.body.password, user.password, function(err, pass) {
-                        if (pass) {
-                            req.session.user = user.firstName;
-                            req.session.email = user.email;
-                            req.session.role = user.role;
-                            req.session.entity_id = user.entity_id;
-                            req.session.entity = user.name;
-                            req.session.type = user.type;
-                            return res.redirect("/dashboard")
-                        } else {
-                            req.flash('alert', 'Email or Password entered is Invalid, please try again');
-                            return res.redirect("/")
-                        };
-                    })
+                        bcrypt.compare(req.body.password, user.password, function(err, pass) {
+                            if (pass) {
+                                req.session.user = user.firstName;
+                                req.session.email = user.email;
+                                req.session.role = user.role;
+                                req.session.entity_id = user.entity_id;
+                                req.session.entity = user.name;
+                                req.session.type = user.type;
+                                return res.redirect("/dashboard")
+                            } else {
+                                req.flash('alert', 'Email or Password entered is Invalid, please try again');
+                                return res.redirect("/")
+                            };
+                        });
+                    };
                 })
         })
         .catch(function(err) {
