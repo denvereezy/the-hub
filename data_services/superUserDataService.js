3c3ef6d48628e4dfe408420d0676560fbc52905c
denver daniels <denver@projectcodex.co>
2016-04-21 10:14:53 +0200

    user id added to query

--- data_services/superUserDataService.js
+++ data_services/superUserDataService.js
@@ -4,7 +4,7 @@ module.exports = function(connection) {
     const queryService = new QueryService(connection);
 
     this.showRequests = function() {
-        return queryService.executeQuery('select user.id as user_id, firstname, email, name from user inner join entity on user.entity_id = entity.id where status = \'pending\'');
+        return queryService.executeQuery('select user.id as user_id, firstname, email, name, status from user inner join entity on user.entity_id = entity.id where status = \'pending\' or status = \'rejected\'');
     };
 
     this.acceptRequest = function(id) {
