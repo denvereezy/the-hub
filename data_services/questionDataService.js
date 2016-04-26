58b057f2a6ab69a1e5b27d57e58741429dd4e98b
denver daniels <denver@projectcodex.co>
2016-04-26 14:15:32 +0200

    beautified code

--- data_services/questionDataService.js
+++ data_services/questionDataService.js
@@ -1,25 +1,25 @@
 const QueryService = require("../data_services/query-service");
 
 module.exports = function(connection) {
-  const queryService = new QueryService(connection);
+    const queryService = new QueryService(connection);
 
-  this.showAll = function(questionnaire_id) {
-    return queryService.executeQuery('SELECT status, questionnaire_metric.id as questionnaire_metric_id, metric_id, questionnaire_id,base_questionnaire_id, title, description, value from questionnaire_metric inner join questionnaire on questionnaire_metric.questionnaire_id = questionnaire.id inner join metric on metric_id = metric.id where questionnaire_id = ?', questionnaire_id);
-  };
+    this.showAll = function(questionnaire_id) {
+        return queryService.executeQuery('SELECT status, questionnaire_metric.id as questionnaire_metric_id, metric_id, questionnaire_id,base_questionnaire_id, title, description, value from questionnaire_metric inner join questionnaire on questionnaire_metric.questionnaire_id = questionnaire.id inner join metric on metric_id = metric.id where questionnaire_id = ?', questionnaire_id);
+    };
 
-  this.entityMetrics = function(entity_id) {
-    return queryService.executeQuery('SELECT * from metric where entity_id = ?', entity_id);
-  };
+    this.entityMetrics = function(entity_id) {
+        return queryService.executeQuery('SELECT * from metric where entity_id = ?', entity_id);
+    };
 
-  this.editMetric = function(id) {
-    return queryService.executeQuery('SELECT * FROM metric WHERE id = ?', id);
-  };
+    this.editMetric = function(id) {
+        return queryService.executeQuery('SELECT * FROM metric WHERE id = ?', id);
+    };
 
-  this.updateMetric = function(data, id) {
-    return queryService.executeQuery('UPDATE metric SET ? WHERE id = ?', [data, id]);
-  };
+    this.updateMetric = function(data, id) {
+        return queryService.executeQuery('UPDATE metric SET ? WHERE id = ?', [data, id]);
+    };
 
-  this.showFacilitatorCreatedQuestions = function(entity_id) {
-    return queryService.executeQuery('SELECT metric.id as metric_id, title, description, entity_id from metric inner join entity on metric.entity_id = entity.id where entity.id = ?', entity_id);
-  };
+    this.showFacilitatorCreatedQuestions = function(entity_id) {
+        return queryService.executeQuery('SELECT metric.id as metric_id, title, description, entity_id from metric inner join entity on metric.entity_id = entity.id where entity.id = ?', entity_id);
+    };
 };
