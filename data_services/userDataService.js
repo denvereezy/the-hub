7daf72c938961688d675793c1d7234bf4db10f81
denver daniels <denver@projectcodex.co>
2016-04-26 14:16:55 +0200

    beautified code

--- data_services/userDataService.js
+++ data_services/userDataService.js
@@ -3,35 +3,35 @@ const Promise = require('bluebird');
 const bcrypt = require('bcrypt');
 
 var encryptPassword = function(password) {
-  return new Promise(function(resolve, reject) {
-    bcrypt.genSalt(10, function(err, salt) {
-      bcrypt.hash(password, salt, function(err, hash) {
-        if (err) {
-          return reject(err);
-        }
-        resolve(hash)
-      });
+    return new Promise(function(resolve, reject) {
+        bcrypt.genSalt(10, function(err, salt) {
+            bcrypt.hash(password, salt, function(err, hash) {
+                if (err) {
+                    return reject(err);
+                }
+                resolve(hash)
+            });
+        });
     });
-  });
 };
 
 module.exports = function(connection) {
-  const queryService = new QueryService(connection);
+    const queryService = new QueryService(connection);
 
-  this.showEntityUsers = function(entity_id, email) {
-    return queryService.executeQuery('select * from user where entity_id = ? and email != ?', [entity_id, email]);
-  };
+    this.showEntityUsers = function(entity_id, email) {
+        return queryService.executeQuery('select * from user where entity_id = ? and email != ?', [entity_id, email]);
+    };
 
-  this.deleteUser = function(id) {
-    return queryService.executeQuery('delete from user where id = ?', id);
-  };
+    this.deleteUser = function(id) {
+        return queryService.executeQuery('delete from user where id = ?', id);
+    };
 
-  this.confirmAccount = function(user, token) {
-    return encryptPassword(user.password)
-      .then(function(encryptedPassword) {
-        user.password = encryptedPassword;
-        return queryService.executeQuery('update user set ? where token = ?', [user, token]);
-      });
-  };
+    this.confirmAccount = function(user, token) {
+        return encryptPassword(user.password)
+            .then(function(encryptedPassword) {
+                user.password = encryptedPassword;
+                return queryService.executeQuery('update user set ? where token = ?', [user, token]);
+            });
+    };
 
 };
