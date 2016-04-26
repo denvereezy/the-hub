fc75f1ea239b9a0f46c08d4dce12d4acfe05e943
denver daniels <denver@projectcodex.co>
2016-04-26 14:15:59 +0200

    beautified code

--- data_services/setupQuestionnaireDataService.js
+++ data_services/setupQuestionnaireDataService.js
@@ -1,26 +1,26 @@
 const QueryService = require("../data_services/query-service");
 
 module.exports = function(connection) {
-  const queryService = new QueryService(connection);
+    const queryService = new QueryService(connection);
 
-  this.create = function(data) {
-    return queryService.executeQuery('INSERT INTO questionnaire set ?', data);
-  };
+    this.create = function(data) {
+        return queryService.executeQuery('INSERT INTO questionnaire set ?', data);
+    };
 
-  this.fetchEntityMetrics = function(entity_id) {
-    return queryService.executeQuery('SELECT * FROM metric WHERE entity_id = ?', [entity_id]);
-  };
+    this.fetchEntityMetrics = function(entity_id) {
+        return queryService.executeQuery('SELECT * FROM metric WHERE entity_id = ?', [entity_id]);
+    };
 
-  this.addMetricToMetricTable = function(data) {
-    return queryService.executeQuery('INSERT INTO metric set ?', data);
-  };
+    this.addMetricToMetricTable = function(data) {
+        return queryService.executeQuery('INSERT INTO metric set ?', data);
+    };
 
-  this.linkMetricToQuestionnaire = function(data) {
-    return queryService.executeQuery('INSERT INTO questionnaire_metric set ?', data);
-  };
+    this.linkMetricToQuestionnaire = function(data) {
+        return queryService.executeQuery('INSERT INTO questionnaire_metric set ?', data);
+    };
 
-  this.getQuestionnaireById = function(id) {
-    return queryService.executeQuery('SELECT * FROM questionnaire WHERE id = ?', [id]);
-  };
-  
+    this.getQuestionnaireById = function(id) {
+        return queryService.executeQuery('SELECT * FROM questionnaire WHERE id = ?', [id]);
+    };
+
 };
