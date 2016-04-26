052bc75ed0a77d34c3d60ff605a2fb56b13da9f6
denver daniels <denver@projectcodex.co>
2016-04-26 14:14:43 +0200

    beautified code

--- data_services/loginDataService.js
+++ data_services/loginDataService.js
@@ -3,26 +3,26 @@ const QueryService = require("../data_services/query-service");
 const bcrypt = require('bcrypt');
 
 module.exports = function(connection) {
-  const queryService = new QueryService(connection);
+    const queryService = new QueryService(connection);
 
-  this.signup = function(data) {
-    return new Promise(function(resolve, reject) {
-      bcrypt.genSalt(10, function(err, salt) {
-        bcrypt.hash(data.Password, salt, function(err, hash) {
-          if (err) {
-            return err;
-          }
-          data.Password = hash;
-          connection.query('insert into user set ?', data, function(err, results) {
-            if (err) return reject(err);
-            resolve(results);
-          });
+    this.signup = function(data) {
+        return new Promise(function(resolve, reject) {
+            bcrypt.genSalt(10, function(err, salt) {
+                bcrypt.hash(data.Password, salt, function(err, hash) {
+                    if (err) {
+                        return err;
+                    }
+                    data.Password = hash;
+                    connection.query('insert into user set ?', data, function(err, results) {
+                        if (err) return reject(err);
+                        resolve(results);
+                    });
+                });
+            });
         });
-      });
-    });
-  };
+    };
 
-  this.login = function(email) {
-    return queryService.executeQuery('SELECT * from user inner join entity on entity_id = entity.id WHERE email=?', [email]);
-  };
+    this.login = function(email) {
+        return queryService.executeQuery('SELECT * from user inner join entity on entity_id = entity.id WHERE email=?', [email]);
+    };
 };
