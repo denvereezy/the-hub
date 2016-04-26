e98965275cf2cd10ba2d0df7054967a1b2c8cbd5
denver daniels <denver@projectcodex.co>
2016-04-26 14:16:21 +0200

    beautified code

--- data_services/signupDataService.js
+++ data_services/signupDataService.js
@@ -3,30 +3,30 @@ const Promise = require('bluebird');
 const bcrypt = require('bcrypt');
 
 var encryptPassword = function(password) {
-  return new Promise(function(resolve, reject) {
-    bcrypt.genSalt(10, function(err, salt) {
-      bcrypt.hash(password, salt, function(err, hash) {
-        if (err) {
-          return reject(err);
-        }
-        resolve(hash)
-      });
+    return new Promise(function(resolve, reject) {
+        bcrypt.genSalt(10, function(err, salt) {
+            bcrypt.hash(password, salt, function(err, hash) {
+                if (err) {
+                    return reject(err);
+                }
+                resolve(hash)
+            });
+        });
     });
-  });
 };
 
 module.exports = function(connection) {
-  const queryService = new QueryService(connection);
+    const queryService = new QueryService(connection);
 
-  this.addEntity = function(entity) {
-    return queryService.executeQuery('insert into entity set ?', entity);
-  };
+    this.addEntity = function(entity) {
+        return queryService.executeQuery('insert into entity set ?', entity);
+    };
 
-  this.addUser = function(user) {
-    return encryptPassword(user.password)
-      .then(function(encryptedPassword) {
-        user.password = encryptedPassword;
-        return queryService.executeQuery('insert into user set ?', user);
-      });
-  };
+    this.addUser = function(user) {
+        return encryptPassword(user.password)
+            .then(function(encryptedPassword) {
+                user.password = encryptedPassword;
+                return queryService.executeQuery('insert into user set ?', user);
+            });
+    };
 };
