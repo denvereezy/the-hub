18d40d4cbe777fa3e71a4ff0df38ecbec17b6380
denver daniels <denver@projectcodex.co>
2016-04-27 09:25:46 +0200

    email service updated

--- routes/superUser.js
+++ routes/superUser.js
@@ -25,49 +25,37 @@ exports.handleRequest = function(req, res, next) {
         .then(function(services) {
             var id = req.params.user_id;
             var status;
+            var mailOpts, smtpConfig;
 
+            smtpConfig = nodemailer.createTransport('SMTP', {
+                service: 'Gmail',
+                auth: {
+                    user: 'APP EMAIL',
+                    pass: 'APP PASSWORD'
+                }
+            });
             if (req.body.status === 'Accept') {
                 status = 'active';
-                var mailOpts, smtpConfig;
-
-                smtpConfig = nodemailer.createTransport('SMTP', {
-                    service: 'Gmail',
-                    auth: {
-                        user: 'APP EMAIL',
-                        pass: 'APP PASSWORD'
-                    }
-                });
-
                 mailOpts = {
                     from: 'Findivity',
-                    to: data.email,
-                    subject: 'invite to join'
-                    // text: data.firstName 'you÷r request to join Findivity was succesful. Please setup your password using the link. Note your email address will be used to login ' + 'http://hub.projectcodex.co/account/verifyaccount/' + data.token
+                    to: req.params.email,
+                    subject: 'Request to join'
+                    text: req.params.firstname + ' your request to join Findivity was succesful. Please setup your password using the link. Note your email address will be used to login ' + 'http://hub.projectcodex.co/account/verifyaccount/' + data.token
                 };
 
             } else if (req.body.status === 'Reject') {
                 status = 'rejected';
-                var mailOpts, smtpConfig;
-
-                smtpConfig = nodemailer.createTransport('SMTP', {
-                    service: 'Gmail',
-                    auth: {
-                        user: 'APP EMAIL',
-                        pass: 'APP PASSWORD'
-                    }
-                });
-
                 mailOpts = {
                     from: 'Findivity',
-                    to: data.email,
-                    subject: 'invite to join'
-                    // text: data.firstName 'you÷r request to join Findivity was succesful. Please setup your password using the link. Note your email address will be used to login ' + 'http://hub.projectcodex.co/account/verifyaccount/' + data.token
+                    to: req.params.email,
+                    subject: 'Request to join'
+                    text: req.params.firstname + ' your request to join Findivity was unsuccesful.'
                 };
             };
             const superUserDataService = services.superUserDataService;
             superUserDataService.handleRequest(status, id)
                 .then(function(results) {
-                  smtpConfig.sendMail(mailOpts);
+                    smtpConfig.sendMail(mailOpts);
                     res.redirect('/root');
                 })
                 .catch(function(err) {
